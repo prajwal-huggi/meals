d222e95f00b4cfd3fdfceca9a51af5f86e13cb19
Prajwal <prajwalhuggi02@gmail.com>
2024-10-17 13:58:53 +0530

    Day 1 of the project

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      //clipBehavior is used because the stack doesn't allow the radius to be
+      //in circular so to enforce it clipBhehavior is used.
+      //It actually do is that it crops the part of the image which goes beyond
+      //the shape
+      clipBehavior: Clip.hardEdge,
+      //Elevation is used to give the 3d look to the card
+      elevation: 2,
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 44),
+                color: Colors.black45,
+                child: Column(children: [
+                  Text(meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
